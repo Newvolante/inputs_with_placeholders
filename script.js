154deb04c28cb37e540660c577128271b8396652
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-17 11:13:39 +0200

    created selectors for the 3 main fields

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 (function() {
     console.warn('Script correctly linked to web page');
+
+    const nameField = document.forms[0].elements.name;
+    const mailField = document.forms[0].elements.email;
+    const dateField = document.forms[0].elements.date;
 })();
